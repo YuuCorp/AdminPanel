4ad5d35e8a352ebfe9c1adb7354931286d5e7404
Morgan Tali <lasn.mine@gmail.com>
2024-02-13 21:58:42 +0100

    Add TailwindCSS

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: {
+          100: "#18181A",
+          200: "#161618",
+        },
+        primary: "#FF4D6D",
+        text: {
+          100: "#ACACAE",
+          200: "#8A8A8A",
+        },
+        accent: "#28262B",
+      },
+    }
+  },
+  plugins: [],
+} satisfies Config
