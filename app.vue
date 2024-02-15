44f91313ecd579604625698bba49fe5008b0b61d
crackheadakira <lasn.mine@gmail.com>
2024-02-15 01:14:19 +0100

    Wrap pagination arrows in button for accessibility

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-background-200 h-screen w-screen p-8 flex flex-col gap-4 font-jetbrains uppercase text-text-100">
+  <div class="bg-background-200 h-screen w-screen p-8 flex flex-col items-center justify-center gap-4 font-jetbrains uppercase text-text-100">
     <csm-bento class="flex w-full gap-4 justify-center">
       <LogView />
       <div class="flex flex-col min-h-full justify-between">
--- components/theAnnouncements.vue
+++ components/theAnnouncements.vue
@@ -3,9 +3,13 @@
     <div class="flex w-full justify-between items-center">
       <h1 class="text-lg font-bold">Announcements</h1>
       <div class="flex gap-1 items-center">
-        <Icon @click="$emit('pageChange', 'back')" name="material-symbols:chevron-left" class="text-primary inline-block cursor-pointer"/>
+        <Button>
+          <Icon @click="$emit('pageChange', 'back')" name="material-symbols:chevron-left" class="text-primary inline-block cursor-pointer"/>
+        </Button>
         <p class="text-sm select-none">{{ props.currentPage }} / 2</p>
-        <Icon @click="$emit('pageChange', 'forward')" name="material-symbols:chevron-right" class="text-primary inline-block cursor-pointer"/>
+        <Button>
+          <Icon @click="$emit('pageChange', 'forward')" name="material-symbols:chevron-right" class="text-primary inline-block cursor-pointer"/>
+        </Button>
       </div>
     </div>
     <csm-divider class="h-[2px] w-full bg-accent rounded-full"></csm-divider>
